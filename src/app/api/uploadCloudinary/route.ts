4b2605eba38b67b91170308b14eb90710349c45a
Denis <deniscotecata@gmail.com>
2024-03-02 13:45:11 +0200

    Revert to original route.ts to upload and update to node 20.x

--- src/app/api/uploadCloudinary/route.ts
+++ src/app/api/uploadCloudinary/route.ts
@@ -13,40 +13,32 @@ cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_API,
   api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_API_SECRET,
+  secure: true,
 });
 
 export async function POST(req: Request) {
   if (req.method === "POST") {
-    try {
-      const formData = await req.formData();
-      const formDataArray = Array.from(formData);
-      const uploadedImageUrls = [];
-      let resource = "";
-      let name = "";
+    const formData = await req.formData();
+    const formDataArray = Array.from(formData);
+    const uploadedImageUrls = [];
+    let resource = "";
+    let name = "";
 
-      if (typeof File !== "undefined") {
-        for (const [key, value] of formDataArray) {
-          if (key === "resource") {
-            resource = value.toString();
-          } else if (key === "name") {
-            name = value.toString();
-          } else if (value instanceof File) {
-            const file = value;
-            const b64 = Buffer.from(await file.arrayBuffer()).toString("base64");
-            let dataURI = "data:" + file.type + ";base64," + b64;
-            const uploadedImage = await cloudinary.uploader.upload(dataURI, {
-              folder: `Blackwater/${resource}/${name}`,
-            });
-            uploadedImageUrls.push(uploadedImage.secure_url);
-          }
-        }
-        return NextResponse.json({ uploadedImageUrls }, { status: 200 });
-      } else {
-        return NextResponse.json({ error: "error" }, { status: 500 });
+    for (const [key, value] of formDataArray) {
+      if (key === "resource") {
+        resource = value.toString();
+      } else if (key === "name") {
+        name = value.toString();
+      } else if (value instanceof File) {
+        const file = value;
+        const b64 = Buffer.from(await file.arrayBuffer()).toString("base64");
+        let dataURI = "data:" + file.type + ";base64," + b64;
+        const uploadedImage = await cloudinary.uploader.upload(dataURI, {
+          folder: `Blackwater/${resource}/${name}`,
+        });
+        uploadedImageUrls.push(uploadedImage.secure_url);
       }
-    } catch (e) {
-      console.error(e);
-      return NextResponse.json({ e }, { status: 500 });
     }
+    return NextResponse.json({ uploadedImageUrls }, { status: 200 });
   }
 }
