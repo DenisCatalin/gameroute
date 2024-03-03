0c821e46d231d30595a91bcc6ad9caca871751d1
Denis <deniscotecata@gmail.com>
2024-03-03 15:35:55 +0200

    Maybe fix payload too large error 413

--- src/app/api/uploadCloudinary/route.ts
+++ src/app/api/uploadCloudinary/route.ts
@@ -1,14 +1,6 @@
 import { NextResponse } from "next/server";
 import { v2 as cloudinary } from "cloudinary";
 
-export const dynamic = "auto";
-export const dynamicParams = true;
-export const revalidate = false;
-export const fetchCache = "auto";
-export const runtime = "nodejs";
-export const preferredRegion = "auto";
-export const maxDuration = 5;
-
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_API,
@@ -31,14 +23,49 @@ export async function POST(req: Request) {
         name = value.toString();
       } else if (value instanceof File) {
         const file = value;
-        const b64 = Buffer.from(await file.arrayBuffer()).toString("base64");
-        let dataURI = "data:" + file.type + ";base64," + b64;
-        const uploadedImage = await cloudinary.uploader.upload_large(dataURI, {
-          folder: `Blackwater/${resource}/${name}`,
-        });
-        uploadedImageUrls.push(uploadedImage.secure_url);
+
+        if (file.size > 5000000) {
+          const fileSize = file.size;
+          const chunkSize = 5000000;
+          let start = 0;
+          let end = Math.min(chunkSize, fileSize);
+          let partNumber = 1;
+
+          while (start < fileSize) {
+            const blob = file.slice(start, end);
+            const reader = new FileReader();
+
+            reader.readAsDataURL(blob);
+            reader.onloadend = async () => {
+              const b64Data = reader.result?.toString() || "";
+              const dataURI = "data:" + file.type + ";base64," + b64Data;
+
+              const uploadedImage = await cloudinary.uploader.upload_large(dataURI, {
+                folder: `Blackwater/${resource}/${name}`,
+                partNumber,
+                totalParts: Math.ceil(fileSize / chunkSize),
+              });
+
+              uploadedImageUrls.push(uploadedImage.secure_url);
+
+              if (end < fileSize) {
+                start = end;
+                end = Math.min(start + chunkSize, fileSize);
+                partNumber++;
+              }
+            };
+          }
+        } else {
+          const b64 = Buffer.from(await file.arrayBuffer()).toString("base64");
+          let dataURI = "data:" + file.type + ";base64," + b64;
+          const uploadedImage = await cloudinary.uploader.upload_large(dataURI, {
+            folder: `Blackwater/${resource}/${name}`,
+          });
+          uploadedImageUrls.push(uploadedImage.secure_url);
+        }
       }
     }
+
     return NextResponse.json({ uploadedImageUrls }, { status: 200 });
   }
 }
