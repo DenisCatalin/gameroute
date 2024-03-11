a0bee0e46c0aa574bcef35e9fa78a6046ef1fb5a
Denis <deniscotecata@gmail.com>
2024-03-11 19:32:11 +0200

    GRP-10: fix build

--- src/app/api/checkToken/route.ts
+++ src/app/api/checkToken/route.ts
@@ -1,7 +1,7 @@
 import { verifyToken } from "@/app/lib/verifyToken";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export const dynamic = "auto";
 export const dynamicParams = true;
@@ -11,13 +11,16 @@ export const runtime = "nodejs";
 export const preferredRegion = "auto";
 export const maxDuration = 5;
 
+type MyTokenPayload = JwtPayload & {
+  exp: number;
+};
+
 export async function GET(req: Request) {
   if (req.method === "GET") {
     const cookieStore = cookies();
     if (cookieStore.get("token")) {
-      const token = await verifyToken(cookieStore.get("token")?.value);
-      //@ts-ignore
-      if (token?.exp > Date.now() / 1000) {
+      const token = (await verifyToken(cookieStore.get("token")?.value)) as MyTokenPayload;
+      if (token.exp > Date.now() / 1000) {
         return NextResponse.json({ token }, { status: 200 });
       } else {
         cookieStore.delete("token");
